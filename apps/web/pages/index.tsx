fa372369ae3888eb3bb3ab1d1efe94488f244856
Omar Diab <me@omardiab.com>
2021-12-16 16:43:19 +0900

    repro

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,19 @@
 import { Button } from "ui";
+import dynamic from "next/dynamic"
 
+const CountryFlagSvg = ({ countryCode }: {countryCode: string}) => {
+  const Component = dynamic(
+    () => import(`country-flag-icons/react/3x2/${countryCode}`),
+    { loading: () => <span className="fallback" /> }
+  );
+  return <Component />;
+};
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      <CountryFlagSvg countryCode="US" />
     </div>
   );
 }
